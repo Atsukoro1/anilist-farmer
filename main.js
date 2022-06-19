3c35b67532de475c88e06e8e8595d19697e9c16d
Atsukoro1 <dornicakkuba@gmail.com>
2022-06-19 21:42:08 +0200

    Main part of the bot

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 import Client from "./client.js";
+import { CronJob } from "cron";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const cl = new Client(process.env.TOKEN);
+
+const users = new Map();
+
+const findMoreUsers = () => {
+
+};
+
+const followLastUser = () => {
+    
+};
+
+/**
+ * This function will run every 10 seconds
+ * 
+ * @returns {void}
+ */
+const tick = () => {
+    if(users.size == 0) {
+        findMoreUsers();
+    } else {
+        followLastUser();
+    }
+};
+
 cl.login()
 .then(async() => {
-    cl.fetchGlobal({
-        page
-    })
+    const job = new CronJob(
+        "*/10 * * * * *",
+        tick,
+        null,
+        true,
+        'Europe/Prague'
+    );
+
+    job.start();
+
+    process.stdout.write(`Bot started!\n`);
 })
 .catch(err => {
-    console.log(err);
+    process.stderr.write(`Error happened while starting the bot: ${err}\n`)
 })
